d2b0769db2a11f8587f3291456197de45dbc7b3b
IceDanielC <1119526092@qq.com>
2024-10-24 21:17:46 +0800

    fix: 类型

--- src/utils/google.ts
+++ src/utils/google.ts
@@ -7,7 +7,7 @@ export const googleSearch = async (searchKey: string) => {
       `${baseurl}?key=${googleSearchKey}&cx=${googleCxId}&q=${searchKey}&c2coff=1&start=1&end=10&dateRestrict=m[1]`
     );
     const data = await response.json();
-    return data.items.map((item) => item.snippet).join("\n"); // 返回搜索结果
+    return data.items.map((item: { snippet: string; }) => item.snippet).join("\n"); // 返回搜索结果
   } catch (error) {
     console.error("搜索错误:", error);
     return "搜索结果出现异常，请忽略";
